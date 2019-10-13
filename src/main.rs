ad556ae0be746e44fedc17f182ec60be13dd6c00
Joël Schaerer <joel.schaerer@laposte.net>
2019-10-13 19:24:39 +0200

    implement weight increase

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::fs::File;
 use std::fmt;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Entry {
     weight: f32,
     path: String
 }
+
+type Entries<'a> = HashMap<String, f32>;
 
 impl fmt::Display for Entry {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -26,26 +29,40 @@ impl std::str::FromStr for Entry {
     }
 }
 
-fn parse_file_contents(buf:&str) -> Vec<Entry> {
+fn parse_file_contents(buf:&str) -> Entries {
     buf.lines()
-        .filter_map(|l| l.parse::<Entry>().ok())
+        .filter_map(|l| {
+            let entry = l.parse::<Entry>();
+            match entry {
+                Err(_) => None,
+                Ok(e) => Some((e.path, e.weight))
+            }})
         .collect()
 }
 
-fn parse_file(fname:&str) -> std::io::Result<Vec<Entry>> {
+fn parse_file(fname:&str) -> std::io::Result<Entries> {
     let mut aj_file = File::open(fname)?;
     let mut contents = String::new();
     aj_file.read_to_string(&mut contents)?;
     Ok(parse_file_contents(contents.as_str()))
 }
 
+fn increase_weight(entries: &mut Entries, path: &str) {
+    if let Some(weight) = entries.get_mut(path) {
+        *weight += 1f32;
+    } else {
+        entries.insert(path.to_owned(), 1f32);
+    }
+}
+
 fn main()  -> std::io::Result<()>{
     let aj_fname: & 'static str = "/home/joel/.local/share/autojump/autojump.txt";
-    let entries = parse_file(aj_fname)?;
-    for e in &entries {
-        println!("{}", e);
+    let mut entries = parse_file(aj_fname)?;
+    increase_weight(&mut entries, "tuto");
+    increase_weight(&mut entries, "tuto");
+    for (p,w) in &entries {
+        println!("{} : {}", p, w);
     }
-    println!("#3: {}", entries[3].weight);
     Ok(())
 }
 
@@ -56,7 +73,9 @@ mod tests {
     fn read_test_file() {
         let entries = parse_file("tests/fine.txt");
         assert!(!entries.is_err());
-        assert_eq!(3, entries.unwrap().len());
+        let entries = entries.unwrap();
+        assert_eq!(3, entries.len());
+        assert_eq!(Some(&5f32), entries.get("titi"));
     }
 
     #[test]
